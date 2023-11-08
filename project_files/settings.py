5305d55bb282bf90e4395c04ec9da29b5d4becbe
belemaaa <graceitamunoala@gmail.com>
2023-11-08 16:56:10 +0000

    mysql config

--- project_files/settings.py
+++ project_files/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'project_files.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'electricity_consumption_system',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': '',
     }
 }
 
