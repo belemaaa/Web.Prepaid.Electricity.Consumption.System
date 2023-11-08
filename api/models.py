ea5830a9e709a62c8c16235c47956f20bfdc2750
belemaaa <graceitamunoala@gmail.com>
2023-11-08 20:00:39 +0000

    auth migrations

--- api/models.py
+++ api/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
 class User(AbstractUser):
     phone_number = models.CharField(max_length=20)
     def __str__(self):
@@ -9,11 +9,11 @@ class User(AbstractUser):
     
 class Electricity_Plan(models.Model):
     name = models.CharField(max_length=255, null=False, blank=False)
-    price = models.DecimalField(decimal_places=2, default='0.00', null=False, blank=False)
+    price = models.DecimalField(decimal_places=2, max_digits=50, default='0.00', null=False, blank=False)
     def __str__(self):
         return f'{self.name}'
     
 class Electricity_Pin(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
-    electricity_plan = models.ForeignKey(Electricity_Plan, on_delete=models.SET_NULL)
+    electricity_plan = models.ForeignKey(Electricity_Plan, on_delete=models.CASCADE)
     pin = models.CharField(max_length=10)
