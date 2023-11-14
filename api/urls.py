47fa4735e4de4f02b15a80d364a964c612691346
belemaaa <graceitamunoala@gmail.com>
2023-11-14 20:01:36 +0000

    configured urls

--- api/urls.py
+++ api/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('login/', views.Login.as_view()),
     path('electricity_plans/create', views.Electricity_Plan.as_view()),
     path('electricity_plans/view', views.Electricity_Plan.as_view()),
-    path('payment/<int:plan_id>/', views.Payment.as_view())
+    path('payment/<int:plan_id>/', views.Payment.as_view()),
+    path('profile/', views.Profile.as_view()),
+    path('paid_plans/', views.Retrieve_Paid_Plans.as_view()),
+    path('consumption_reader/', views.Consumption_Reader.as_view())
 ]
