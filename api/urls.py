4631e68fb8ecb6f888a5f83be76dde5c7b838747
belemaaa <graceitamunoala@gmail.com>
2023-11-17 01:02:43 +0000

    delete endpoint - for admin

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('login/', views.Login.as_view()),
     path('electricity_plans/create', views.Electricity_Plan.as_view()),
     path('electricity_plans/view', views.Electricity_Plan.as_view()),
+    path('electricity_plans/<int:plan_id>/delete', views.Electricity_Plan.as_view()),
     path('payment/<int:plan_id>/', views.Payment.as_view()),
     path('profile/', views.Profile.as_view()),
     path('paid_plans/', views.Retrieve_Paid_Plans.as_view()),
