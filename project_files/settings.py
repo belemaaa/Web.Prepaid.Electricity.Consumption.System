4f828da24adff56bd481ba646bc1340822d88216
belemaaa <graceitamunoala@gmail.com>
2023-11-18 17:11:19 +0000

    updated some settings

--- project_files/settings.py
+++ project_files/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'project_files.urls'
@@ -139,3 +140,7 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated"
     ],
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000"
+]
